5b3f36c114171192d444d90ae9c611f69a04abfd
Steven <beatla2day@gmail.com>
2022-02-27 20:28:52 +0700

    added paypal api missing client id and secret

--- routes/confirmation.js
+++ routes/confirmation.js
@@ -6,4 +6,70 @@ router.get('/', (req, res) => {
     res.render('confirmation/index')
 })
 
+router.post('/', (req, res) => {
+    const create_payment_json = {
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "redirect_urls": {
+            "return_url": "confirmation/index",
+            "cancel_url": "/"
+        },
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "Fake Onward Ticket",
+                    "sku": "001",
+                    "price": "4.00",
+                    "currency": "USD",
+                    "quantity": 1
+                }]
+            },
+            "amount": {
+                "currency": "USD",
+                "total": "4.00"
+            },
+            "description": "Fake onward ticket just for fun"
+        }]
+    };
+    paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+            throw error;
+        } else {
+            for (let i = 0; i < payment.link.length; i++) {
+                if (payment.links[i].rel === 'approval_url') {
+                    res.redirect(payment.links[i].href)
+                }
+            }
+        }
+    });
+    
+})
+
+router.get('/', (req, res) => {
+    const payerID = req.query.payerID
+    const paymentID = req.query.paymentID
+
+    const execute_payment_json = {
+        "payer_id": payerID,
+        "transactions": [{
+            "amount": {
+                "currency": "USD",
+                "total": "4.00"
+            }
+        }]
+    };
+    paypal.payment.execute(paymentID, execute_payment_json, function (error, payment) {
+        if (error) {
+            console.log(error.response);
+            throw error;
+        } else {
+            console.log("Get Payment Response");
+            console.log(JSON.stringify(payment));
+        }
+    });
+})
+
+
 module.exports = router
--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const axios = require('axios').default
 const apiSearch = require('./public/javascript/middleware/apiSearch.js')
+const paypal = require('paypal-rest-sdk')
 
+paypal.configure({
+    'mode': 'sandbox', //sandbox or live
+    'client_id': 'EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM',
+    'client_secret': 'EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM'
+  });
 
 const indexRouter = require('./routes/index')
 const searchRouter = require('./routes/searchresults')
