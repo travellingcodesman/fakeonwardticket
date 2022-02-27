fbc66272e084b47fd7f1a126d73d356dda092130
Steven <beatla2day@gmail.com>
2022-02-28 01:04:39 +0700

    client id and secret in .env

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const paypal = require('paypal-rest-sdk')
 
 paypal.configure({
     'mode': 'sandbox', //sandbox or live
-    'client_id': 'EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM',
-    'client_secret': 'EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM'
+    'client_id': process.env.PAYPAL_CLIENT_ID,
+    'client_secret': process.env.PAYPAL_CLIENT_SECRET
   });
 
 const indexRouter = require('./routes/index')
